718599807b83856ad8e37d2a2c49b651e9abd58f
M.Rani <M.Rani>
2023-04-24 16:14:27 +0530

    FC

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Date from './Date'
+
+function App() {
+  return (
+    <div className="App">
+     <Date/>
+    </div>
+  );
+}
+
+export default App;
